488d0312e804de0bd0965c8f3395b29c9eae4ea0
Zaknefeinn <zaknefeinn@gmail.com>
2018-05-12 20:30:03 -0500

    debug

--- server.js
+++ server.js
@@ -113,6 +113,7 @@ if (process.env.NODE_ENV === 'production') {
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    console.log(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
